597645eda3388a216ae5f7dab6346b325bc3ee11
D. Prameswara <meshwara@gmail.com>
2022-11-17 16:03:58 +0700

    Initial Commit

---
+++ Conditional Modifier/Conditional_ModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conditional_ModifierApp.swift
+//  Conditional Modifier
+//
+//  Created by D. Prameswara on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Conditional_ModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
